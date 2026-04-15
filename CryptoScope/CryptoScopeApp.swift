0a66052d12d4cd6ef9233b4f0f4ac4deb9b26a26
Anatolii <anatolii.semenchuk@schindler.com>
2026-04-15 13:06:53 +0300

    Initial Commit

---
+++ CryptoScope/CryptoScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoScopeApp.swift
+//  CryptoScope
+//
+//  Created by Anatolii Semenchuk on 15.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct CryptoScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
